c2720a8bdfed4d4ced0d49e9036da56aea717c31
Adam Haynes <adamhay@microsoft.com>
2018-06-19 10:52:48 +0200

    custom task scaffolding

---
+++ .AppVeyor/CustomAppVeyorTasks.psm1
@@ -0,0 +1,31 @@
+﻿function Invoke-CustomAppveyorInstallTask
+{
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param ()
+
+    Write-Info -Message 'Custom Install Task Begin.'
+
+    Write-Info -Message 'Custom Install Task Complete.'
+}
+
+function Start-CustomAppveyorTestTask
+{
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param ()
+
+    Write-Info -Message 'Custom Test Task Begin.'
+
+    Write-Info -Message 'Custom Test Task Complete.'
+}
+
+function Start-CustomAppveyorAfterTestTask
+{
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param ()
+
+    Write-Info -Message 'Custom After Test Task Begin.'
+
+    Write-Info -Message 'Custom After Test Task Complete.'
+}
+
+Export-ModuleMember -Function
